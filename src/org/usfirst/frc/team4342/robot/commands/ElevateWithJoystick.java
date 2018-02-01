766f363b33fa35ed9dc616d519a36edc95e24491
Ernie3 <ernestgwilsoniii@gmail.com>
2018-01-31 21:25:28 -0500

    refactor ElevateWithJoystick

--- src/org/usfirst/frc/team4342/robot/commands/ElevateWithJoystick.java
+++ src/org/usfirst/frc/team4342/robot/commands/ElevateWithJoystick.java
@@ -4,15 +4,22 @@ import org.usfirst.frc.team4342.robot.subsystems.Elevator;
 
 import edu.wpi.first.wpilibj.Joystick;
 
+/**
+ * Command to control the elevator with a joystick
+ */
 public class ElevateWithJoystick extends CommandBase {
-	private static final double DEADBAND = 0.03;
+	private static final double DEADBAND = 0.03, TOP_WINDOW = 70.0, BOTTOM_WINDOW = 5.0;
 	
 	private boolean idle, initializedIdle;
-	private double idleSetpoint;
 	
 	private Joystick joystick;
 	private Elevator elevator;
 	
+	/**
+	 * Command to control the elevator with a joystick
+	 * @param joystick the joystick
+	 * @param elevator the elevator
+	 */
 	public ElevateWithJoystick(Joystick joystick, Elevator elevator) {
 		this.joystick = joystick;
 		this.elevator = elevator;
@@ -28,8 +35,6 @@ public class ElevateWithJoystick extends CommandBase {
 	@Override
 	protected void execute() {
 		final double INPUT = joystick.getTwist();
-		final boolean IN_BOTTOM_WINDOW = elevator.getDistance() < 5;
-		final boolean IN_TOP_WINDOW = elevator.getDistance() > 70;
 		idle = checkJoystickDeadband(INPUT);
 		
 		if(elevator.isAtBottom()) {
@@ -42,12 +47,13 @@ public class ElevateWithJoystick extends CommandBase {
 		}
 		
 		if(!elevator.isAtBottom() && idle && !initializedIdle) {
-			idleSetpoint = elevator.getDistance();
-			elevator.setSetpoint(idleSetpoint);
+			elevator.setSetpoint(elevator.getDistance()); // hold current height
 			initializedIdle = true;
-			return;
 		} 
 		else if(!idle) {
+			final boolean IN_BOTTOM_WINDOW = elevator.getDistance() < BOTTOM_WINDOW;
+			final boolean IN_TOP_WINDOW = elevator.getDistance() > TOP_WINDOW;
+
 			double input = 0.0;
 
 			if(IN_TOP_WINDOW && INPUT > 0)
