992627982184b110195d8fe9becbfd12d7830ce4
Ernie3 <ernestgwilsoniii@gmail.com>
2018-02-21 18:34:06 -0500

    add Swerve Tank with Xbox

--- src/org/usfirst/frc/team4342/robot/commands/swerve/DriveSwerveTankWithJoysticks.java
+++ src/org/usfirst/frc/team4342/robot/commands/swerve/DriveSwerveTankWithJoysticks.java
@@ -4,16 +4,19 @@ import org.usfirst.frc.team4342.robot.commands.TeleopCommand;
 import org.usfirst.frc.team4342.robot.subsystems.SwerveDrive;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 
 /**
  * Command used to control swerve like a tank drive. This should
  * only be used in unfortunate situations...
  */
 public class DriveSwerveTankWithJoysticks extends TeleopCommand {
-    private static final double DEADBAND = 0.02;
+    private static final double DEADBAND = 0.06;
 
 	private boolean idle;
 
+	private XboxController xbox;
 	private Joystick left, right;
 	private SwerveDrive drive;
 	
@@ -37,6 +40,23 @@ public class DriveSwerveTankWithJoysticks extends TeleopCommand {
 	}
 
 	/**
+	 * Drive Swerve "Tank" With Joysticks
+	 * @param xbox the xbox controller
+	 * @param drive the swerve drive
+	 */
+	public DriveSwerveTankWithJoysticks(XboxController xbox, SwerveDrive drive) {
+		this.xbox = xbox;
+		this.drive = drive;
+		
+		this.requires(drive.fr);
+		this.requires(drive.fl);
+		this.requires(drive.rr);
+		this.requires(drive.rl);
+		
+		this.requires(drive);
+	}
+
+	/**
 	 * {@inheritDoc}
 	 */
 	@Override
@@ -49,8 +69,8 @@ public class DriveSwerveTankWithJoysticks extends TeleopCommand {
 	 */
     @Override
     protected void execute() {
-        double leftInput = -left.getY();
-		double rightInput = -right.getY();
+        double leftInput = getLeftInput();
+		double rightInput = getRightInput();
 
 		boolean l = Math.abs(leftInput) > DEADBAND;
 		boolean r = Math.abs(rightInput) > DEADBAND;
@@ -74,5 +94,13 @@ public class DriveSwerveTankWithJoysticks extends TeleopCommand {
 	@Override
 	protected void end() {
 		drive.stop();
+	}
+
+	private double getLeftInput() {
+		return xbox != null ? -xbox.getY(Hand.kLeft) : -left.getY();
+	}
+
+	private double getRightInput() {
+		return xbox != null ? -xbox.getY(Hand.kRight) : -right.getY();
 	}
 }
